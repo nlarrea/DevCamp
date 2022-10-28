6b3bd324c5be99794a05fde69e5933e53a50949e
NLarrea <larrea.naia@gmail.com>
2022-10-28 12:26:08 +0200

    3-JAVASCRIPT 7-assignmentOperators.js added

---
+++ 3-JAVASCRIPT/7-assignmentOperators.js
@@ -0,0 +1,13 @@
+var name = 'Naia'; // this is an assignment
+
+// we can also do this
+var sum = 0;
+var gradeOne = 100;
+var gradeTwo = 80;
+//sum = gradeOne + gradeTwo; // this would work correctly
+sum += gradeOne;
+console.log(sum); // 100
+sum += gradeTwo;
+console.log(sum); // 180
+
+// the same can be done with the rest of the arithmetic operators
