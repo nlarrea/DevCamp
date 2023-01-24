9b9b471f916f78ef2cd2b646de34dd17f125e24c
nlarrea <larrea.naia@gmail.com>
2023-01-24 19:30:29 +0100

    02-full-stack/module_02-python/ strings added

--- 02-full-stack/module_02-python/01_data_types.py
+++ 02-full-stack/module_02-python/01_data_types.py
@@ -29,6 +29,27 @@ print(receipt)
 
 # both give us the same response
 
+# CASE-FUNCTIONS
+sentence = "the quick brown fox jumped over the lazy dog"
+print(sentence)
+print(sentence.capitalize())    # first letter of the string in uppercase
+print(sentence.title())         # first letter of every word from the string in uppercase
+print(sentence.upper())
+print(sentence.lower())
+print(sentence)                 # original string doesn't get modified
+
+# ACCESS TO PART OF THE STRING
+sentence = "The quick brown fox jumped"
+print(sentence[0])              # T
+print(sentence[12])             # o
+# we can't do this:
+# sentence[1] = 'r' -> Python doesn't let change the value of a char from a string
+print(sentence[0:5])            # The q -> the range doesn't take the last char, it only takes the first 4 instead of 5
+first_word = sentence[0:3]
+print(first_word)               # The
+new_sentence = "thy" + sentence[3:]     # takes all the sentence from the 4th char untill the end (start counting from 0 -> 4th char = index 3)
+print(new_sentence)             # thy quick brown fox jumped
+
 
 
 # Bytes and byte arrays
